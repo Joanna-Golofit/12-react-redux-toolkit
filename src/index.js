7cd7a905e186b2156ba177172ce7f7cd8b875715
Joanna-Golofit <sunderlajsa@gmail.com>
2022-01-13 15:22:20 +0100

    zaczynamy

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+// import { combineReducers } from "@reduxjs/toolkit";
+// import { createReducer } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
